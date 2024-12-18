c356b8dd9271cf0d7e884383e7719eea2dd95426
Morgane Bonillo <mobonill@student.42nice.fr>
2024-12-18 15:56:44 +0100

    dvfg

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: mobonill <mobonill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:22:27 by mobonill          #+#    #+#             */
-/*   Updated: 2024/12/17 17:15:13 by mobonill         ###   ########.fr       */
+/*   Updated: 2024/12/18 15:55:38 by mobonill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,35 @@ void update_env(t_env *env, const char *var_name, const char *new_value)
 		{
 			free(env->value);
 			env->value = ft_strdup(new_value);
-			printf("NEW %s now %s\n", env->name, env->value);
+			printf("NEW %s now %s\n", env->name, env->value); // del
 			return;
 		}
 		env = env->next;
 	}
+	// printf("bash: Environment variable %s not found. ", var_name);
+	// printf("Please implement insertion if needed.\n");
 }
 
-void update_paths_and_free(char *current_path, char *new_path, t_env *env)
+void cd_to_path(const char *path, t_env *env)
 {
+	char *current_path = getcwd(NULL, 0);
+	char *new_path = NULL;
+
+	if (!current_path)
+		current_path = ft_strdup(get_env_value("PWD", env));
+	if (!current_path)
+	{
+		printf("bash: cd: unable to determine current directory\n");
+		return;
+	}
+
+	if (chdir(path) == -1)
+	{
+		printf("bash: cd: %s: No such file or directory\n", path);
+		free(current_path);
+		return;
+	}
+
 	new_path = getcwd(NULL, 0);
 	if (!new_path)
 	{
@@ -40,29 +60,6 @@ void update_paths_and_free(char *current_path, char *new_path, t_env *env)
 	update_env(env, "PWD", new_path);
 	free(current_path);
 	free(new_path);
-}
-
-void cd_to_path(const char *path, t_env *env)
-{
-	char *current_path;
-	char *new_path;
-
-	current_path = getcwd(NULL, 0);
-	new_path = NULL;
-	if (!current_path)
-		current_path = ft_strdup(get_env_value("PWD", env));
-	if (!current_path)
-	{
-		printf("bash: cd: unable to determine current directory\n");
-		return;
-	}
-	if (chdir(path) == -1)
-	{
-		printf("bash: cd: %s: No such file or directory\n", path);
-		free(current_path);
-		return;
-	}
-	update_paths_and_free(current_path, new_path, env);
 }
 
 void cd_to_home(t_env *env)
@@ -78,19 +75,6 @@ void cd_to_home(t_env *env)
 	cd_to_path(home_path, env);
 }
 
-void cd_to_previous(t_env *env)
-{
-	char *old_path;
-
-	old_path = get_env_value("OLDPWD", env);
-	if (!old_path)
-	{
-		printf("bash: cd: OLDPWD not set\n");
-		return;
-	}
-	cd_to_path(old_path, env);
-}
-
 int builtin_cd(t_simple_cmds *cmd, t_env *env)
 {
 	int arg_count;
@@ -104,11 +88,10 @@ int builtin_cd(t_simple_cmds *cmd, t_env *env)
 	{
 		printf("bash: cd: too many arguments\n");
 		g_global_exit = 1;
+		return (1);
 	}
-	else if (arg_count == 1 || (arg_count == 2 && ft_strcmp(cmd->str[1], "--") == 0))
+	else if (arg_count == 1)
 		cd_to_home(env);
-	else if (arg_count == 2 && ft_strcmp(cmd->str[1], "-") == 0)
-		cd_to_previous(env);
 	else
 		cd_to_path(cmd->str[1], env);
 	return (0);
